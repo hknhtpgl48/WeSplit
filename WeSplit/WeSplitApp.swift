8fb0c27ef0abb5fc745381a13edb087dcdaa509d
hknhtpgl48 <hkn.hatipolu48@gmail.com>
2023-12-30 23:35:39 +0300

    we split project added

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Hakan Hatipoğlu on 29.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
