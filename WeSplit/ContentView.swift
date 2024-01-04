5f1e8490b3d643f151f4eee5d900d83cc1c7c700
hknhtpgl48 <hkn.hatipolu48@gmail.com>
2024-01-04 10:43:48 +0300

    custom modifier added for total amount

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -52,6 +52,7 @@ struct ContentView: View {
                 }
                 Section("Total Amount") {
                     Text(totalAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .foregroundStyle(tipPercentage == 0 ? .red : .black)
                 }
                 Section("Amount per Person") {
                     Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
